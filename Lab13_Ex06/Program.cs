117f1f5daea33873c60bef9e926dadfd8713d04a
newyork <spidermanwin1@gmail.com>
2024-03-31 03:00:11 +0700

    ex6

---
+++ Lab13_Ex06/Program.cs
@@ -0,0 +1,57 @@
+﻿Television myTV = new SonyTV();
+myTV.Wattage = 100;
+myTV.TurnOn();
+myTV.ChannelUp();
+myTV.ChannelDown();
+myTV.TurnOff();
+
+Lamp myLamp = new DesktopLamp();
+myLamp.Wattage = 50;
+myLamp.TurnOn();
+myLamp.ChannelUp();
+myLamp.ChannelDown();
+myLamp.TurnOff();
+
+interface IRemoteControl
+{
+    public void TurnOn();
+    public void TurnOff();
+    public void ChannelUp();
+    public void ChannelDown();
+}
+abstract class PowerAppliance
+{
+    public bool PowerStatus;
+    public int Wattage;
+}
+class Television : PowerAppliance , IRemoteControl
+{
+    public void TurnOn() { System.Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public void TurnOff() { System.Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public void ChannelUp() { System.Console.WriteLine("TV Channel up"); }
+    public void ChannelDown() { System.Console.WriteLine("TV Channel down"); }
+}
+
+class Lamp : PowerAppliance , IRemoteControl
+{
+    public void TurnOn() { System.Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public void TurnOff() { System.Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+    public void ChannelUp() { System.Console.WriteLine("Lamp cannot change channel "); }
+    public void ChannelDown() { System.Console.WriteLine("Lamp cannot change channel"); }
+}
+
+
+class SonyTV : Television, IRemoteControl
+{
+    public void TurnOn() { System.Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public void TurnOff() { System.Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public void ChannelUp() { System.Console.WriteLine("TV Channel up"); }
+    public void ChannelDown() { System.Console.WriteLine("TV Channel down"); }
+}
+class DesktopLamp : Lamp, IRemoteControl
+{
+    public void TurnOn() { System.Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public void TurnOff() { System.Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+    public void ChannelUp() { System.Console.WriteLine("Lamp cannot change channel"); }
+    public void ChannelDown() { System.Console.WriteLine("Lamp cannot change channel"); }
+}
